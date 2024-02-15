54165770333c7956ff854c4bdb1d7006e3b2fa1b
Thiago Terra <thiagoterradev@gmail.com>
2024-02-15 00:43:15 -0300

    Creating and adding styles to the orange background button

---
+++ src/components/ButtonOrangeBackground.jsx
@@ -0,0 +1,23 @@
+import styles from "./ButtonOrangeBackground.module.css";
+import { useNavigate } from "react-router-dom";
+
+const ButtonOrangeBackground = (props) => {
+
+    const navigate = useNavigate();
+
+    let pagina = "/";
+
+    if (props.path === "sobre") {
+        pagina = "/sobre";
+    } else if (props.path === "porquecontratar") {
+        pagina = "/porquecontratar";
+    } else if (props.path === "contato") {
+        pagina = "/contato";
+    }
+
+    return (
+        <button className={styles.buttonOrangeBgClass} onClick={() => navigate(pagina)}>{props.name}</button>
+    );
+};
+
+export default ButtonOrangeBackground;
