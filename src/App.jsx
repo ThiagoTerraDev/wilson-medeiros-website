92864ad6cc7d9509e2401c3507e7d2873169c595
Thiago Terra <thiagoterradev@gmail.com>
2024-07-12 16:22:49 -0300

    adds new route contato

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Contato from "./pages/Contato";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -12,6 +13,7 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/contato" element={<Contato />} />
           </Routes>
           <Footer />
         </BrowserRouter>
