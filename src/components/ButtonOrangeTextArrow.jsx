2f6fb07ba31c7f6380dbc207edbb30ab4c4ff318
Thiago Terra <thiagoterradev@gmail.com>
2024-07-20 22:20:58 -0300

    button adjusts

--- src/components/ButtonOrangeTextArrow.jsx
+++ src/components/ButtonOrangeTextArrow.jsx
@@ -3,24 +3,28 @@ import { useNavigate } from "react-router-dom";
 
 const ButtonOrangeTextArrow = (props) => {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    let pagina = "/";
+  let pagina = "/";
 
-    if (props.path === "sobre") {
-        pagina = "/sobre";
-    } else if (props.path === "porquecontratar") {
-        pagina = "/porquecontratar";
-    } else if (props.path === "contato") {
-        pagina = "/contato";
-    }
-
-    return (
-        <div className={styles.divButtonOrangeText}>
-            <button className={styles.buttonOrangeText} onClick={() => navigate(pagina)}>{props.name}</button>
-            <img src="/arrow-component.svg" alt="" />
-        </div> 
-    );
+  if (props.path === "sobre") {
+    pagina = "/sobre";
+  } else if (props.path === "porquecontratar") {
+    pagina = "/porquecontratar";
+  } else if (props.path === "contato") {
+    pagina = "/contato";
+  } else if (props.path === "salestalksoque") {
+    pagina = "/salestalksoque";
+  } else if (props.path === "salestalksmanifesto") {
+    pagina = "/salestalksmanifesto";
+  }
+  
+  return (
+    <div className={styles.divButtonOrangeText}>
+      <button className={styles.buttonOrangeText} onClick={() => navigate(pagina)}>{props.name}</button>
+      <img src="/arrow-component.svg" alt="" />
+    </div> 
+  );
 };
 
 export default ButtonOrangeTextArrow;
