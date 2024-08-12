40c4ae5a2e5e71b6276b8eef898728d12ab154a2
Thiago Terra <thiagoterradev@gmail.com>
2024-08-12 00:01:41 -0300

    adds new route sobre

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from "./components/Footer";
 import PorqueContratar from "./pages/PorqueContratar";
 import SalesTalksManifesto from "./pages/SalesTalksManifesto";
 import SalesTalksOqueE from "./pages/SalesTalksOqueE";
+import Sobre from "./pages/Sobre";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="/porquecontratar" element={<PorqueContratar />} />
             <Route path="/salestalksoque" element={<SalesTalksOqueE />} />
             <Route path="/salestalksmanifesto" element={<SalesTalksManifesto />} />
+            <Route path="/sobre" element={<Sobre />} />
           </Routes>
           <Footer />
         </BrowserRouter>
